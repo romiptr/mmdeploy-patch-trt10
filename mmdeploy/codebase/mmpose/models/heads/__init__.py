74d4c540c6520656410320fc602fd53dd29380d9
romiptr <romiputrapamungkas@gmail.com>
2026-06-26 16:56:40 +0700

    remove unused files

--- mmdeploy/codebase/mmpose/models/heads/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from . import mspn_head, rtmo_head, simcc_head, yolox_pose_head
-
-__all__ = ['mspn_head', 'yolox_pose_head', 'simcc_head', 'rtmo_head']
